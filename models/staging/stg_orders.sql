7a3ad0ba8089f506defe3603eb7dbb6a1c7df141
ojalbhatnagar21@gmail.com <ojalbhatnagar21@gmail.com>
2025-07-08 16:46:29 +0000

    stg_order

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='view') }}
+
+SELECT
+    invoice_no,
+    stock_code,
+    description,
+    TRY_CAST(quantity AS INT) AS quantity,
+    TO_TIMESTAMP(invoice_date, 'DD/MM/YY HH24:MI') AS invoice_ts,
+    TRY_CAST(unit_price AS FLOAT) AS unit_price,
+    TRY_CAST(customer_id AS INT) AS customer_id,
+    country
+FROM {{ source('raw', 'orders_raw') }}
